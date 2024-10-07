a262fccc5fc7412edfdc3454b191b7e99dea6da7
Anuj Jambhale <86155650+jambhaleAnuj@users.noreply.github.com>
2024-10-07 20:51:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,11 @@ export default function App() {
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          
+          <div className="text-center justify-center mb-10 sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Made by Anuj Jambhale
+            </div>
+          </div>
           <div className="text-center">
             <h1 className=" font-bold tracking-wide text-3xl text-gray-900 sm:text-6xl">
               CipherCraft <br />
